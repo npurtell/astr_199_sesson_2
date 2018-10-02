8b12f78be08315390c27c9e73d797daabab17446
npurtell <43651652+npurtell@users.noreply.github.com>
2018-10-02 11:26:47 -0700

    beginning python scripts
    learning how to do various things in python

---
+++ operators.py
@@ -0,0 +1,38 @@
+x = 9		#make x equal 9
+y = 3		#make y equal 3
+
+#arithmetic operators
+print(x+y)	#addition
+print(x-y)	#subtraction
+print(x*y)	#multiplication
+print(x/y)	#divison
+print(x%y)	#modulus 
+print(x**y)	#exponentiation 
+x = 9.91823
+print(x//y) #floor division, gets rid of extra decimals
+
+#assignment operators 
+x = 9 	#sets x equal to 9
+x += 3	#sets x equal to x + 3
+print(x)
+x = 9	#sets x back equal to 9
+x -= 3	#set x = x - 3
+print(x)
+x = 9
+x *= 3 	#set x = x * 3
+print(x)
+x = 9
+x /=3	#set x = x / 3
+print(x)
+x **=3	#set x = x ** 3
+print(x)
+
+#comparison operators
+x = 9
+y = 3
+print(x==y)	#will print true if x equals y, false otherwise 
+print(x!=y)	#will print false if x=y, true otherwise 
+print(x>y)	#true if x>y, false otherwise
+print(x<y)	#true if x<y, false otherwise 
+print(x>=y)	#true if x is greater or equal to y, false otherwise
+print(x<=y) #true if x os less or equal to y, false otherwise
